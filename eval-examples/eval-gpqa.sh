633f282ee018bee0aa566bfeaf4fd4dd9c4122ef
yubin <yubin.email@163.com>
2025-04-14 19:48:24 +0800

    :bug: 修复 shell 脚本中 OUTPUT_DIR 错误的 bug

--- eval-examples/eval-gpqa.sh
+++ eval-examples/eval-gpqa.sh
@@ -3,7 +3,7 @@
 MODEL=/path/to/your/model
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,tensor_parallel_size=4,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 
-OUTPUT_DIR=eval-results/gpqa
+OUTPUT_DIR=results/gpqa
 
 TASK=gpqa:diamond
 
